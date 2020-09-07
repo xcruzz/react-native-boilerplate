3e7a2f1128a1a82eec15b1287929420ce18679c9
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-09-06 23:01:01 -0700

    Adding Photo scene to be used for logoRecognition Uses monochromatic logo :)

---
+++ src/scenes/photo/Photo.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  StyleSheet, Text, View, StatusBar,
+} from 'react-native'
+import Button from 'components/Button'
+import ActiveDisplay from 'components/ActiveDisplay'
+import { colors, images } from 'theme'
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.darkGris,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: 'white',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: 'white',
+    alignContent: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+})
+
+const Photo = ({ navigation }) => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+    <ActiveDisplay
+      title="El Auditor"
+      activityDisplay={images.logo_mono}
+      onPress={() => {}}
+    />
+    <Text style={styles.text}>
+      Sube una foto a ver si el auditor le encuentra algo...
+    </Text>
+
+    <Button
+      title="Buscar mis fotos"
+      color="white"
+      backgroundColor={colors.gray}
+      onPress={() => {
+        navigation.navigate('Details', { from: 'Foto' })
+      }}
+    />
+  </View>
+)
+
+Photo.propTypes = {
+  navigation: PropTypes.shape({ navigate: PropTypes.func }),
+}
+
+Photo.defaultProps = {
+  navigation: { navigate: () => null },
+}
+
+export default Photo
