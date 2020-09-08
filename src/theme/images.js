9ada0c4183f168ae591ff7fff7c8f01964a473fa
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-09-07 18:30:42 -0700

    Restructuring Image  resources

--- src/theme/images.js
+++ src/theme/images.js
@@ -9,11 +9,16 @@ export const svgs = {
 
 // png/jpeg
 export const images = {
-  logo_blanco: require('../../assets/images/mvcLogoBlanco.png'),
-  logo_negro: require('../../assets/images/mvcLogoNegro.png'),
-  logo_arcoiris: require('../../assets/images/L.png'),
-  logo_mono: require('../../assets/images/mvcLogoMono.png'),
+  logo_negro: require('../../assets/images/mvclogo/mvcLogoNegro.png'),
+  logo_azul: require('../../assets/images/mvclogo/mvcLogoAzul.png'),
+  logo_rojo: require('../../assets/images/mvclogo/mvcLogoRojo.png'),
+  logo_verde: require('../../assets/images/mvclogo/mvcLogoVerde.png'),
+  logo_oro: require('../../assets/images/mvclogo/mvcLogoOro.png'),
+  logo_blanco: require('../../assets/images/mvclogo/blancoiris7.png'),
+  logo_arcoiris: require('../../assets/images/mvclogo/blancoiris1.png'),
+  logo_mono: require('../../assets/images/mvclogo/mvcLogoMono.png'),
   spinner_arcoiris: require('../../assets/images/spinnerL.gif'),
+  loader_blancoiris: require('../../assets/images/loader_blancoiris.gif'),
 }
 
 // image preloading
