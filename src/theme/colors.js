7ebddc10e5ade409f4f0ad8ac31a57cae31fbe41
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-09-09 04:28:50 -0700

    Adding new color light gray and Changing details to Spanish

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -2,7 +2,7 @@ const colors = {
   darkPurple: '#231d54',
   purple: '#8100ff',
   lightPurple: '#9388db',
-  lightGrayPurple: '#f7f7fb',
+  lightGray: '#f0f0f0',
   pink: '#ff3d69',
   gray: '#797777',
   negro: '#000000',
