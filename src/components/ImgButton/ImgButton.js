dd21d1faf9a96ec9b6620b806ffcf6e287d6a731
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-09-25 03:10:26 -0700

    Uncalled for isDisable capability for ImgButton  :)

--- src/components/ImgButton/ImgButton.js
+++ src/components/ImgButton/ImgButton.js
@@ -2,13 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Linking, ImageBackground, Button } from 'react-native'
 
-const ImgButton = ({ onPressURL, imgSource, styles }) => {
+const ImgButton = ({ onPressURL, imgSource, styles, isDisabled }) => {
   return (
     <ImageBackground style={styles} source={imgSource}>
       <Button
         title=""
         onPress={() => {
-          Linking.openURL(onPressURL)
+          if (!isDisabled) Linking.openURL(onPressURL)
         }}
       />
     </ImageBackground>
@@ -19,12 +19,14 @@ ImgButton.propTypes = {
   onPressUrl: PropTypes.string,
   imgSource: PropTypes.number,
   styles: PropTypes.object,
+  isDisabled: PropTypes.bool,
 }
 
 ImgButton.defaultProps = {
   onPressUrl: '',
   imgSource: null,
   styles: {},
+  isDisabled: false,
 }
 
 export default ImgButton
