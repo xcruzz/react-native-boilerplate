48325fff3b01ed77ccfb75e58459ac0eba7026be
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-10-03 22:04:31 -0400

    Fonts

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -9,6 +9,14 @@ export const fonts = {
     bold: 'openSans_bold',
     boldItalic: 'openSans_bold_italic',
   },
+  NeuePlak: {
+    Black: 'neuePlak_Black',
+    Bold: 'neuePlak_Bold',
+    CompBold: 'neuePlak_CompBold',
+    CondExtraBlack: 'neuePlak_CondExtraBlack',
+    Regular: 'neuePlak_Regular',
+    WideExtraBlack: 'neuePlak_WideExtraBlack',
+  },
 }
 
 // fonts preloading
@@ -30,6 +38,24 @@ export const fontsAll = [
   },
   {
     openSans_bold_italic: require('../../assets/fonts/OpenSans-BoldItalic.ttf'),
+  },
+  {
+    neuePlak_Black: require('../../assets/fonts/NeuePlak-Black.ttf'),
+  },
+  {
+    neuePlak_Bold: require('../../assets/fonts/NeuePlak-Bold.ttf'),
+  },
+  {
+    neuePlak_CompBold: require('../../assets/fonts/NeuePlak-CompBold.ttf'),
+  },
+  {
+    neuePlak_CondExtraBlack: require('../../assets/fonts/NeuePlak-CondExtraBlack.ttf'),
+  },
+  {
+    neuePlak_Regular: require('../../assets/fonts/NeuePlak-Regular.ttf'),
+  },
+  {
+    neuePlak_WideExtraBlack: require('../../assets/fonts/NeuePlak-WideExtraBlack.ttf'),
   },
 ]
 export const fontAssets = fontsAll.map((x) => Font.loadAsync(x))
