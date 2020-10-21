d13a756aa6e399e43859e8d47fe582de1789122f
Xavier Cruz Urbina <xavier.cruz@nordstrom.com>
2020-10-21 16:23:44 -0400

    L20 Beta - iOS

--- src/scenes/alugaro/ALugaro.js
+++ src/scenes/alugaro/ALugaro.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
 })
 
 const welcomeScreen = globalStyles.welcomeScreen
-const isFirst = 'mvcApp:v5.10.21'
+const isFirst = 'mvcApp:v7.10.21'
 
 export default class ALugaro extends React.Component {
   async componentDidMount() {
